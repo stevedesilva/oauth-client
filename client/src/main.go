bbab832d17d8b67bea99be2608f77a10b1b3797c
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2020-08-19 09:51:48 +0100

    adding branch for client

--- client/src/main.go
+++ client/src/main.go
@@ -1,1 +1,19 @@
-package src
+package main
+
+import (
+	"fmt"
+	"github.com/labstack/gommon/log"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Oauth client")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w,"Hello Oauth client!")
+	})
+	err := http.ListenAndServe(":8080",nil)
+	if err != nil {
+		log.Error(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/silvade/oauth-client
 
 go 1.14
+
+require github.com/labstack/gommon v0.3.0 // indirect
